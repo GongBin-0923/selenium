442a21f021a3126eb1998ce0f23be4e0f122a4b5
GongBin-0923 <1287513533@qq.com>
2020-09-04 16:38:37 +0800

    appium_作业第二次

--- test_appium/testcase/test_appium_zuoye2.py
+++ test_appium/testcase/test_appium_zuoye2.py
@@ -89,24 +89,32 @@ class TestAppiumWeWork1():
         # 点击保存
         self.driver.find_element(MobileBy.ID, "com.tencent.wework:id/hvk").click()
         sleep(1)
+        # 判断是否添加成功
         assert "添加成功" in self.driver.page_source
         print("添加成功")
 
     def test_delete_member(self):
+        # 点击通讯录
         locator = (MobileBy.XPATH, "//*[@resource-id='com.tencent.wework:id/eas' and @text='通讯录' ]")
         el3 = self.driver.find_element_by_xpath("//*[@resource-id='com.tencent.wework:id/eas' and @text='通讯录' ]")
         WebDriverWait(self.driver, 5).until(expected_conditions.element_to_be_clickable(locator))
         el3.click()
-
+        # 选取所有包含“测试name”的元素
         ui_locator = 'new UiSelector().textContains("测试name")'
         elememts = self.driver.find_elements(MobileBy.ANDROID_UIAUTOMATOR, ui_locator)
+        # 当存在的元素大于0时候进行删除
         if len(elememts) > 0:
             for element in elememts:
+                # 点击联系人
                 element.click()
+                # 点击右上角功能按钮
                 de_locator = (MobileBy.ID, "com.tencent.wework:id/hvd")
                 WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable(de_locator))
                 self.driver.find_element(*de_locator).click()
+                # 点击编辑成员
                 self.driver.find_element(MobileBy.ID, "com.tencent.wework:id/b87").click()
+                # 点击删除成员
                 self.driver.find_element(MobileBy.ID, "com.tencent.wework:id/efq").click()
+                # 删除确认
                 self.driver.find_element(MobileBy.ID, "com.tencent.wework:id/bit").click()
                 return self.test_delete_member()
